1f6dff679a6b2df5b9cdc59e121987c789f742cf
Zahran000 <zahranc04@gmail.com>
2025-02-07 08:44:47 +0500

    Update Task_4.cpp

--- LAB_2/Task_4.cpp
+++ LAB_2/Task_4.cpp
@@ -1,1 +1,30 @@
+## Task 4: Dynamic String Array
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int numStrings;
+    cout << "Enter the number of strings: ";
+    cin >> numStrings;
+
+    string* strings = new string[numStrings];
+
+    cout << "Enter the strings:\n";
+    for (int i = 0; i < numStrings; ++i) {
+        cin.ignore();
+        getline(cin, strings[i]);
+    }
+
+    sort(strings, strings + numStrings);
+
+    cout << "\nSorted Strings:\n";
+    for (int i = 0; i < numStrings; ++i) {
+        cout << strings[i] << endl;
+    }
+
+    delete[] strings;
+    return 0;
+}
