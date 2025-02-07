81a0e414c31dbb259e4af99bec89822245a7b7da
Zahran000 <zahranc04@gmail.com>
2025-02-07 08:48:54 +0500

    Update Task_9.cpp

--- LAB_2/Task_9.cpp
+++ LAB_2/Task_9.cpp
@@ -1,1 +1,44 @@
+Task 9: Real-World Use Case - Product Inventory
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Product {
+    int productID;
+    string name;
+    int quantity;
+    float price;
+};
+
+int main() {
+    int numProducts;
+    cout << "Enter the number of products: ";
+    cin >> numProducts;
+
+    Product* products = new Product[numProducts];
+
+    for (int i = 0; i < numProducts; ++i) {
+        cout << "Enter details for product " << i + 1 << ":\n";
+        cout << "Product ID: ";
+        cin >> products[i].productID;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, products[i].name);
+        cout << "Quantity: ";
+        cin >> products[i].quantity;
+        cout << "Price: ";
+        cin >> products[i].price;
+    }
+
+    float totalValue = 0;
+    for (int i = 0; i < numProducts; ++i) {
+        totalValue += products[i].quantity * products[i].price;
+    }
+
+    cout << "\nTotal inventory value: " << totalValue << endl;
+
+    delete[] products;
+    return 0;
+}
 
