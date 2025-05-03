63291209dbd18abbd7738e0644d5f0905fbe8864
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2025-05-04 04:57:59 +0500

    Create Task_5.cpp

---
+++ Lab_10/Task_5.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class FileReader {
+private:
+    ifstream inputFile;
+    string filename;
+
+public:
+    FileReader(const string& fname) : filename(fname) {}
+
+    void readFile() {
+        inputFile.open(filename);
+
+        if (!inputFile.is_open()) {
+            cerr << "Error: Could not open file '" << filename << "'" << endl;
+            return;
+        }
+
+        string line;
+
+        while (getline(inputFile, line)) {
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
+
+            istringstream iss(line);
+            string Type, ID, Name, YearStr, ExtraData, Certification;
+            int Year;
+
+            getline(iss, Type, ',');
+            getline(iss, ID, ',');
+            getline(iss, Name, ',');
+            getline(iss, YearStr, ',');
+            getline(iss, ExtraData, ',');
+            getline(iss, Certification, ',');
+
+            Year = stoi(YearStr);
+
+            if (Type == "AutonomousCar") {
+                size_t colonPos = ExtraData.find(':');
+                if (colonPos != string::npos) {
+                    string versionStr = ExtraData.substr(colonPos + 1);
+                    float softwareVersion = stof(versionStr);
+                    cout << "AutonomousCar ID: " << ID << " Software Version: " << softwareVersion << endl;
+                }
+            }
+            else if (Type == "ElectricVehicle") {
+                size_t colonPos = ExtraData.find(':');
+                if (colonPos != string::npos) {
+                    string capacityStr = ExtraData.substr(colonPos + 1);
+                    int batteryCapacity = stoi(capacityStr);
+                    cout << "ElectricVehicle ID: " << ID << " Battery Capacity: " << batteryCapacity << endl;
+                }
+            }
+            else if (Type == "HybridTruck") {
+                size_t colonPos = ExtraData.find(':');
+                size_t pipePos = ExtraData.find('|');
+                
+                if (colonPos != string::npos && pipePos != string::npos) {
+                    string cargoStr = ExtraData.substr(colonPos + 1, pipePos - colonPos - 1);
+                    int cargo = stoi(cargoStr);
+                    
+                    size_t batteryColonPos = ExtraData.find(':', pipePos);
+                    if (batteryColonPos != string::npos) {
+                        string batteryStr = ExtraData.substr(batteryColonPos + 1);
+                        int battery = stoi(batteryStr);
+                        cout << "HybridTruck ID: " << ID << " Cargo: " << cargo << " Battery: " << battery << endl;
+                    }
+                }
+            }
+            else {
+                cerr << "Error: Unknown vehicle type '" << Type << "' for ID: " << ID << endl;
+            }
+
+            cout << "---------------------" << endl;
+        }
+
+        inputFile.close();
+    }
+};
+
+int main() {
+    FileReader fileReader("vehicles.txt");
+    fileReader.readFile();
+    return 0;
+}
