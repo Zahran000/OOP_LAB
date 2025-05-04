3316d1acceddce7c7b53fe039e5219f3d323888c
Syed Zahran Ali Chishti <zahranc04@gmail.com>
2025-05-04 05:27:14 +0500

    Update Task_7.cpp

--- Lab_10/Task_7.cpp
+++ Lab_10/Task_7.cpp
@@ -6,24 +6,14 @@ using namespace std;
 int main() {
     ofstream outFile("sensor_log.txt");
     
-    if (!outFile) {
-        cerr << "Error opening file" << endl;
-        return 1;
-    }
-
-    outFile << "Sensor1: 25.5C" << endl;
+    outFile << "Sensor 1: 25.5 C" << endl;
     streampos pos1 = outFile.tellp();
-    cout << "Posion after first write: " << pos1 << endl;
+    cout << pos1 << endl;
 
-    outFile << "Sensor2: 72.3F" << endl;
+    outFile << "Sensor 2: 98.1 %RH" << endl;
     streampos pos2 = outFile.tellp();
-    cout << "Position fter second write: " << pos2 << endl;
-
-    outFile << "Sensor3: 101.2kPa" << endl;
-    streampos pos3 = outFile.tellp();
-    cout << "Position after third wrte: " << pos3 << endl;
+    cout << pos2 << endl;
 
     outFile.close();
-
     return 0;
 }
